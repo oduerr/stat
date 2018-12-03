25920d52d41254ccf2bcd4ea1a698fdc9d0569f4
Oliver4242 <oliver.duerr@gmail.com>
2018-12-03 08:44:26 +0100

    Mean and Variance

--- 03_wkeit/Summe_von_ZV.R
+++ 03_wkeit/Summe_von_ZV.R
@@ -1,15 +1,19 @@
 x = rep(NA, 10000)
-wid = 3
+wid = 2
 for (i in 1:length(x)){
   x[i] = sum(sample(1:6, wid, replace = TRUE))
 }
+mean(x)
+var(x)
 plot(table(x), type='h')
 
 
 
 x = rep(NA, 10000)
-wid = 3
+wid = 1
 for (i in 1:length(x)){
   x[i] = sum(runif(wid, 0, 6))
 }
-hist(x)
+mean(x)
+var(x)
+hist(x, freq = FALSE, 30)
