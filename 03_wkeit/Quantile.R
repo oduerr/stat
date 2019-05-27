08eeb907982b85663b94fea09ce67b339f500ca2
Oliver4242 <oliver.duerr@gmail.com>
2019-05-27 15:39:51 +0200

    small changes

--- 03_wkeit/Quantile.R
+++ 03_wkeit/Quantile.R
@@ -2,11 +2,27 @@ source("http://www-home.htwg-konstanz.de/~oduerr/data/nym.2002.R")
 d = hist(nym.2002$time, freq=F, 50, xlab="Zeit in Minuten")
 males <- nym.2002[nym.2002$gender == "Male",]$time
 females <- nym.2002[nym.2002$gender == "Female",]$time
+hist(females)
 d = ecdf(males)
+plot(d)
 
 par(mfcol=c(1,2))
 plot(ecdf(females), do.points=FALSE, main='')
-plot(pnorm(100:600,mean(females), sd(females)), type = 'l', xlab='time [minutes]')
+plot(100:600, pnorm(100:600,mean(females), sd(females)), type = 'l', xlab='time [minutes]')
 par(mfcol=c(1,1))
 
 plot(NULL, xlim = c(200,500), ylim=c(200,500), xlab='Theoretische Quantile', ylab='Empirische Quantile')
+
+
+qs = seq(0.1,0.99, 0.01) #c(0.1, 0.25, 0.5, 0.75, 0.9)
+q_exp = quantile(females, qs)
+q_theo = qnorm(p=qs,mean(females), sd(females))
+plot(0:400,pnorm(0:400,mean(females), sd(females)))
+
+plot(q_theo, q_exp)
+
+library(car)
+qqPlot(females)
+d = rnorm(1000, mean=42, sd=10)
+d = rexp(10000, rate=34)
+qqPlot(d, distribution = 'exp')
--- 03_wkeit/Summe_von_ZV.R
+++ 03_wkeit/Summe_von_ZV.R
@@ -1,18 +1,18 @@
 x = rep(NA, 10000)
-wid = 2
+wid = 100
 for (i in 1:length(x)){
   x[i] = sum(sample(1:6, wid, replace = TRUE))
 }
 mean(x)
 var(x)
-plot(table(x), type='h')
+plot(table(x)/length(x), type='h')
 
 
 
 x = rep(NA, 10000)
-wid = 3
+wid = 100
 for (i in 1:length(x)){
-  x[i] = sum(runif(wid, 0, 6))
+  x[i] = mean(runif(wid, 1, 6))
 }
 mean(x)
 var(x)
