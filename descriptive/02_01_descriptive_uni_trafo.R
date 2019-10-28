a19d6e047b2b3898f307a5ec44952a892bc959ad
Oliver4242 <oliver.duerr@gmail.com>
2019-10-28 13:10:48 +0100

    Fixed wrong Values

--- descriptive/02_01_descriptive_uni_trafo.R
+++ descriptive/02_01_descriptive_uni_trafo.R
@@ -17,17 +17,29 @@ mean(X) # 9.94
 sd(X)   # 5.03
 hist(X,100)
 
-Y = -10*X + 5 #Gilt fuer alle Datenpunkte 
+Y = 1.8*X + 32 #Gilt fuer alle Datenpunkte 
 hist(Y, 100)
 mean(Y) # 49.89
 sd(Y)   #  9.05
-sd(X) * 10
+sd(X) * 1.8
+
+
+#################################
+X = rgamma(1000,shape=1)
+hist(X,100)
+z = (X-mean(X))/ sd(X)
+z=scale(X)
+hist(z,100)
+
+
 
 
 #################################
 # 
 library(MASS)
 summary(Boston$crim) 
+
+
 stripchart(Boston$crim, method="jitter")
 stripchart(log10(Boston$crim), method="jitter") 
 stripchart(Boston$crim, method="jitter", log='x') #Not 100% the same due to jitter
