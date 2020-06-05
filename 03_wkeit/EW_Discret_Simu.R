155554a4a595ffd4228dee081ceb7f2931b4965e
Oliver4242 <oliver.duerr@gmail.com>
2020-06-05 18:51:29 +0200

    Ergebnisse

--- 03_wkeit/EW_Discret_Simu.R
+++ 03_wkeit/EW_Discret_Simu.R
@@ -3,16 +3,16 @@ N = 100000
 x = sample(1:6, N, replace = TRUE)
 
 #Abschaetzung von EW
-mean(x)
+mean(x) #3.5052
 
 #Erwartungswerte von X^2
-mean(x^2)
+mean(x^2) #15.21858
 
 #Augenzahl in Euro, falls gerade Zahl
 #x 6 6 2 5 4 4 5 1 4 5
 xg = x %% 2 == 0 #gerade TRUE  TRUE  TRUE FALSE  TRUE  TRUE FALSE FALSE  TRUE FALSE
 #xg * x #6 6 2 0 4 4 0 0 4 0
-mean(xg * x)
+mean(xg * x) #2.00234
 
 ##Augenzahl in Euro zum Quadrat, falls gerade Zahl
-mean(xg * x^2)
+mean(xg * x^2)#9.38124
