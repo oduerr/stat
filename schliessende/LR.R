ad990f4d5ce40ec70b2c7880991fe409c62d61a1
Oliver Dürr <oliver.duerr@gmail.com>
2024-04-08 09:05:56 +0200

    Update LR.R

--- schliessende/LR.R
+++ schliessende/LR.R
@@ -1,8 +1,10 @@
 load("~/Dropbox/__HTWG/Statistic/Stat_HS2018/handouts/inference/LR/unique2010.rda")
 res = lm(Pax ~ ATM, data=unique2010)
-predict(res,data.frame(ATM=c(20100, 24000, 500)))
+predict(res,data.frame(ATM=c(20100, 24000, 5000)))
 plot(Pax ~ ATM, data=unique2010)
 abline(res)
+
+summary(res)
 
 xi = seq(1,10,0.5)
 n = length(xi)
@@ -13,10 +15,38 @@ plot(xi,yi)
 res = lm(yi ~ xi)  
 
 
+##### Daten für LR aus Normaverteilung
+
+inter = slope = rep(NA, 1000)
+for (i in 1:1000) {
+  xs = seq(1,10,0.5/4)
+  steigung = 1.2
+  yachs = 0.5
+  mu = xs * steigung + yachs
+  y = mu
+  y = rnorm(length(xs), mu, 1.42)
+  #plot(xs, y)
+  res = lm(y ~ xs)
+  inter[i] = res$coefficients[1]
+  slope[i] = res$coefficients[2]
+}
+hist(inter,50)
+mean(inter) 
+
+hist(slope,50)
+mean(slope) 
+sd(slope)  #0.122
 
 
 
 
+
+
+
+
+
+
+#
 xi = seq(1,10,0.5)
 n = length(xi)
 inters = rep(NA, 1000)
@@ -24,7 +54,7 @@ slopes = rep(NA, 1000)
 in_ci = 0
 for (i in 1:length(inters)){  
   yi = rnorm(n,mean=1*xi+2, sd = 0.5) #DGP
-  plot(xi, yi)
+  #plot(xi, yi)
   res = lm(yi ~ xi)  
   #abline(res)
   #res
@@ -36,6 +66,9 @@ for (i in 1:length(inters)){
     in_ci = in_ci + 1
   }
 }
+in_ci
+
+mean(slopes)  
 #Change N, sd in DGP
 hist(inters,20)
 mean(inters) 
