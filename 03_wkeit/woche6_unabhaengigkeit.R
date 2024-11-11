14373917c9e7ec69e50a00535541d301c48a17d4
Oliver Dürr <oliver.duerr@gmail.com>
2024-11-11 12:40:42 +0100

    Update woche6_unabhaengigkeit.R

--- 03_wkeit/woche6_unabhaengigkeit.R
+++ 03_wkeit/woche6_unabhaengigkeit.R
@@ -1,9 +1,9 @@
 library(titanic)
 titanic = titanic_train[,c('Survived', 'Pclass')]
-table = table(titanic_train$Survived, titanic_train$Pclass) 
+table = table(titanic_train$Pclass, titanic_train$Survived, ) 
 table
 table / 891
-plot(t(table), main='Beobachtet')
+plot(table, main='Beobachtet')
 
 
 #### Simulation ####
@@ -11,5 +11,5 @@ df = data.frame(
   Survived = sample(c(0,1), size=891, replace = TRUE, prob = c(0.61, 0.39)),
   Pclass = rep(1:3, c(226, 187, 478))
 )
-
-plot(t(table(df)), main='Simuliert mit unabhängiger Überlebensrate')
+tab = table(df$Pclass, df$Survived) 
+plot(t(tab), main='Simuliert mit unabhängiger Überlebensrate')
