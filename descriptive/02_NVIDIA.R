82609ae7ee0ef9de45f90f8281a64e9b239fda04
Oliver Dürr <oliver.duerr@gmail.com>
2025-10-20 09:15:43 +0200

    Removed unnessessay imports

--- descriptive/02_NVIDIA.R
+++ descriptive/02_NVIDIA.R
@@ -1,10 +1,6 @@
 # Pakete
 # install.packages(c("quantmod","ggplot2","dplyr","tidyr","scales"))
 library(quantmod)
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(scales)
 
 # 1) Daten laden: NVDA ab 2020-01-01
 from <- as.Date("2020-01-01")
@@ -13,8 +9,7 @@ getSymbols("NVDA", src = "yahoo", from = from, to = to, auto.assign = TRUE)
 
 # Wir nutzen den Adjusted Close (splits/dividendenbereinigt)
 df <- data.frame(date = index(NVDA),
-                 adj  = as.numeric(Ad(NVDA))) %>%
-  filter(!is.na(adj))
+                 adj  = as.numeric(Ad(NVDA))) 
 
 plot(df$date, df$adj, type="l", main="NVIDIA (Linear)", xlab="Date", ylab="Adjusted Close Price (USD)")
 plot(df$date, log10(df$adj), type="l", main="NVIDIA ", xlab="Date", ylab="log(Adjusted Close Price (USD))")
